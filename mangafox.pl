391708622af593afa1c15324f59f4d5ebfc49d18
Kulag <g.kulag@gmail.com>
2011-04-29 17:17:32 -0400

    Fix grabbing, exit properly, split title/vol/chapter into a tree, and zero-pad page numbers

--- mangafox.pl
+++ mangafox.pl
@@ -1,21 +1,18 @@
 #!/usr/bin/env perl
 package mf;
-
 use v5.10;
-use File::Basename 'dirname';
-use File::Spec;
-use EV;
-use IO::All;
+use AnyEvent::Util;
 use AnyEvent::HTTP;
+use common::sense;
+use IO::All;
+use File::Basename 'dirname';
+use File::Path 'make_path';
+use File::Spec;
 use Mojo::DOM;
-
-my $dir = join '/', File::Spec->splitdir(dirname(__FILE__));
 
 sub DEBUG() { $ENV{DEBUG} // 1 }
 sub CACHE() { $ENV{CACHE} // 1 }
 sub MAX_ACTIVE_REQUESTS() { $ENV{MAX_ACTIVE_REQUESTS} // 5 }
-my $active_requests;
-my $baseurl = 'http://www.mangafox.com';
 
 BEGIN {
 	if (DEBUG) {
@@ -26,7 +23,12 @@ BEGIN {
 	}
 }
 
+my $active_requests;
+my $baseurl = 'http://www.mangafox.com';
 my @backlog;
+my $cv = AE::cv;
+my $dir = join '/', File::Spec->splitdir(dirname(__FILE__));
+
 sub _http($&;$) {
 	if ($active_requests >= MAX_ACTIVE_REQUESTS) {
 		push @backlog, [@_];
@@ -74,51 +76,85 @@ sub http($&;$) {
 
 sub dom($) { Mojo::DOM->new->parse(shift) }
 
-sub mf_manga($) {
-	my $mangaurl = shift;
+sub _make_cb {
+	my $caller_cb = shift;
+	return unless $caller_cb;
+	my $guard = guard { &$caller_cb };
+	sub {$guard};
+}
+
+sub mf_manga($;&) {
+	my ($mangaurl, $cb) = @_;
+	my $chapter_cb = _make_cb($cb);
 	http $mangaurl, sub {
-		dom(shift)->find('a.chico')->each(sub { mf_chapter($baseurl . shift->attrs->{href}) });
+		dom(shift)->find('a.ch')->each(sub {
+			mf_chapter($baseurl . shift->attrs->{href}, $chapter_cb);
+		});
 	};
 }
 
-sub mf_chapter($) {
-	my $page1url = shift;
+sub mf_chapter($;&) {
+	my ($page1url, $cb) = @_;
+	my $guard = guard { &$cb };
+	(my $chapter_url = $page1url) =~ s/[^\/]+$//;
 	http $page1url, sub {
 		my $page1 = dom(shift);
 		my $chapter_title;
 		
 		$page1->find('#image')->each(sub {
-			$chapter_title = shift->attrs->{alt};
+			my $img = shift->attrs;
+			$chapter_title = $img->{alt};
 			$chapter_title =~ tr!\?"/\\<>\|:\*!？”∕＼＜＞｜：＊!;
-			mkdir $chapter_title;
+			$chapter_title =~ s!(.+?) (Vol.\S+) (Ch.[^：]+)： (.+?) at MangaFox.com!$1/$2/$3 - $4!;
+			$chapter_title =~ s/\.$//;
+			unless (-d $chapter_title) {
+				say "Making path: $chapter_title" if DEBUG;
+				make_path $chapter_title;
+			}
+
+			unless (-f "$chapter_title/01.jpg") {
+				_http $img->{src}, sub {
+					my $imgbuf = shift;
+					io("$chapter_title/01.jpg")->binary->print($imgbuf);
+					$guard;
+				};
+			}
 		});
-		
-		$page1->find('select.middle > option')->each(sub {
-			my $pagenum = shift->attrs->{value};
-			
+
+		my @pages;
+		$page1->find('#pgnav > p > a')->each(sub {
+			my $a = shift;
+			push @pages, [$a->text, $a->attrs->{href}];
+		});
+		my $digits = length @pages[-1]->[0];
+		my $spf = '%0' . $digits . 'd';
+		while (my ($pagenum, $href) = @{shift @pages}) {
+			$pagenum = sprintf $spf, $pagenum;
 			return if -f "$chapter_title/$pagenum.jpg";
 			
-			http $page1url . $pagenum . '.html', sub {
-				dom(shift)->find('img#image')->each(sub {
+			http $chapter_url . $href, sub {
+				dom(shift)->find('#image')->each(sub {
 					my $img = shift->attrs;
 					_http $img->{src}, sub {
 						my $imgbuf = shift;
 						io("$chapter_title/$pagenum.jpg")->binary->print($imgbuf);
+						$guard;
 					};
 				});
 			};
-		});
+		}
 	};
 }
 
 sub cli_parse(@) {
 	return say 'No arguments. Paste some mangafox manga urls.' if !@_;
+	my $guard = guard { $cv->send };
 	for (@_) {
 		if (m!$baseurl/manga/\w+/v\d+/c\d+!) {
-			mf_chapter $_;
+			mf_chapter $_, sub {$guard};
 		}
 		elsif (m!$baseurl/manga/\w+!) {
-			mf_manga $_;
+			mf_manga $_, sub {$guard};
 		}
 		else {
 			say "I don't understand '$_'!";
@@ -127,4 +163,5 @@ sub cli_parse(@) {
 }
 
 cli_parse(@ARGV);
-EV::loop;
+$cv->recv;
+
